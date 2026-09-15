b2326ca3a679e2eb2deb5b585a2267d6420fb286
Chinmay Jagtap <chinmay.jagtap@mandg.com>
2026-09-15 14:46:29 +0530

    Revert to static site: replace PHP backend with static index.html for GitHub Pages

--- admin/includes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../includes/bootstrap.php';
-
-function require_admin(): void
-{
-    if (empty($_SESSION['admin_id'])) {
-        redirect('login.php');
-    }
-}
